95863c55a23ff98a9f518fbe4a23b5cd162c7687
diegok <diego@freekeylabs.com>
2019-09-19 16:31:50 +0200

    Working version

--- lib/Locale/Maketext/Extract/Plugin/MojoTemplate.pm
+++ lib/Locale/Maketext/Extract/Plugin/MojoTemplate.pm
@@ -1,24 +1,30 @@
 package Locale::Maketext::Extract::Plugin::MojoTemplate;
+# ABSTRACT: Mojo templates format parser plugin for Locale::Maketext::Extract
+
 use strict;
 use base qw(Locale::Maketext::Extract::Plugin::Base);
 
-# ABSTRACT: Mojo templates format parser plugin for Locale::Maketext::Extract
-
-sub file_types {
-    return [qw( ep )]
-}
+sub file_types {[qw( ep )]}
 
 sub extract {
-    my $self = shift;
-    local $_ = shift;
+    my ($self, $content) = (shift, shift);
+
+    # Match text strings handling embeded quotemarks
+    #TODO: handle q|| qq|| q() etc...
+    my $re_str = qr/
+        (?<quotemark>") (?<text>(?: \\" | [^"] )*) "
+        |
+        (?<quotemark>') (?<text>(?: \\' | [^'] )*) '
+    /x;
 
     my $line = 1;
-
-    while ( /\G(.*?(?:^\s*\%=?([^\n]+)|<\%=?(.+?)\%>))/smg ) {
+    while ( $content =~ /\G( .*? (?:^\s*\%=? \s* ([^\n]+) | <\%=? \s* (.+?) \s* \%>) )/xsmg ) {
         my $code = $2 || $3;
         $line += ( () = ( $1 =~ /\n/g ) );
-        if ( $code =~ /l \s* \( \s* ['"](.+?)['"] \s* (?:,\s*(.+?)) \)/xsm ) {
-            $self->add_entry($1,$line,$2)
+        while ( $code =~ /\G.*?\bl \s* \( \s* $re_str \s* (?:,\s*(?<args>.+?))? \)/xsmg ) {
+            my ($qmark, $text, $args) = map { $+{$_} } qw/ quotemark text args /;
+            $text =~ s/\\$qmark/$qmark/g;
+            $self->add_entry($text,$line,$args)
         }
     }
 
